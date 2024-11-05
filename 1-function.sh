00f0dbd0e222a57f5d24f045783516542205b9a3
meenakshi <meenakshichiluveru249@gmail.com>
2024-11-05 15:49:18 +0530

    dispatch setup

---
+++ 1-function.sh
@@ -0,0 +1,21 @@
+# function is a name given to set of commands
+# syntax for function
+# function(){
+# commands
+# commands
+# }
+
+# or
+
+# abc(){
+# commads
+#{
+
+
+## how to cal a function: simply cal a function like command
+# abc
+
+# example for function
+sample (){
+  echo sample function
+}
