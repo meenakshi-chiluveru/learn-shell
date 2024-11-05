47f2e82720ef4567ac71ed59dc94486c2228f2d5
meenakshi <meenakshichiluveru249@gmail.com>
2024-11-05 18:34:07 +0530

    dispatch setup

---
+++ 2-variables.sh
@@ -0,0 +1,15 @@
+# variable is name given to string
+# syntax =
+# x=100; y=abc;
+# in shell by default there are no data types, it is all a string
+# how to access a variable $varname - $x, $y,$z
+
+# example
+
+name = devops
+faculty = john
+echo welcome to $name training
+echo $name training = faculty $faculty
+
+
+
